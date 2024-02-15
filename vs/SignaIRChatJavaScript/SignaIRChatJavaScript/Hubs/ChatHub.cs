267b5b57877c7ed32bfeb183458f844ae2cce7c6
nikolastefanov <nikoay_stefanov1965@abv.bg>
2024-02-15 20:30:15 +0200

    InitialProgram

---
+++ vs/SignaIRChatJavaScript/SignaIRChatJavaScript/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace SignaIRChatJavaScript.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+
+    public class ChatHub : Hub
+    {
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+
+    }
+}
